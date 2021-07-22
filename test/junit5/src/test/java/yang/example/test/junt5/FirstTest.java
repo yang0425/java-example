824c2f98e48486a92dcee640614327bbf968eb6b
YangYang <vip.yyang@gmail.com>
2021-07-22 12:34:03 +0800

    add junit5 test example.

---
+++ test/junit5/src/test/java/yang/example/test/junt5/FirstTest.java
@@ -0,0 +1,13 @@
+package yang.example.test.junt5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirstTest {
+
+    @Test
+    void assert_equals() {
+        assertEquals(1, 1);
+    }
+}
